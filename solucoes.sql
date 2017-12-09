a504d654499b99948bcb04d784bde8b8ff7271c2
psimoes <p.simoes@campus.fct.unl.pt>
2017-12-09 02:10:32 +0000

    uhm... faltam-me um bocadinho

--- solucoes.sql
+++ solucoes.sql
@@ -32,14 +32,17 @@ FROM compra C2)
 -- Determine a pegada ecológica associada a cada um dos produtos do tipo lar.
 -- eu fiz as contas para a lixivia e acho que os resultados estão bem. De qq maneira enviem um email ao professor a perguntar a pegada ecologica para a lixivia de maneira a confirmar que está certa, ok? Considerei 1% como o correspondente a 1 unidade. Se esta assumpção não estiver correcta, este está mal
 
-select Produto.nome,SUM(Elemento.pegadaEcologica * composto.percentagem) as 'peg' from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Elemento.codigo=composto.elemento where Produto.tipo='lar' group by Produto.nome order by Produto.nome ASC;
+select Produto.nome,SUM(Elemento.pegadaEcologica * composto.percentagem) as 'peg' from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Produto.codigo=composto.produto and Elemento.codigo=composto.elemento where Produto.tipo='lar' group by Produto.nome order by Produto.nome ASC;
+
 
 --Nome do(s) produto(s) mais prejudicial para a saúde – quanto maiores os valores
---no atributo “saúde”, mais prejudiciais são para a mesma
---Assumi o mesmo que em cima, i.e, que é preciso multiplicar a percentagem pela saude. os resultados dão shampoo e termometro o que até parece lógico.
+--no atributo “saúde”, mais prejudiciais são para 
+--a mesma
+--Assumi o mesmo que em cima, i.e, que é preciso multiplicar a percentagem pela saude. os resultados dão gasolina o que parece um resultado lógico.
 --Apesar de achar que dá a resposta certa, quase de certeza que existe uma maneira mais eficiente de realizar esta query. Estou a fazer a mesma pergunta varias vezes. Primeiro descubram se a resposta à pergunta está certa (email prof). Se sim, melhorem a selecao!!!!
+-- lol acabei de ler que não podem fazer sub interrogações na clausula from. Safem-se. não podem usar esta
 
-select nome from (select MAX(pior_para_saude) as pior from (select Produto.nome,SUM(Elemento.saude * composto.percentagem) as pior_para_saude from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Elemento.codigo=composto.elemento where Produto.tipo='lar' group by Produto.nome) as alias) alias2 inner join (select Produto.nome,SUM(Elemento.saude * composto.percentagem) as pior_para_saude from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Elemento.codigo=composto.elemento where Produto.tipo='lar' group by Produto.nome)alias3 where alias2.pior=alias3.pior_para_saude;
+select nome from (select MAX(pior_para_saude) as pior from (select Produto.nome,SUM(Elemento.saude * composto.percentagem) as pior_para_saude from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Produto.codigo=composto.produto and Elemento.codigo=composto.elemento group by Produto.nome) as alias) alias2 inner join (select Produto.nome,SUM(Elemento.saude * composto.percentagem) as pior_para_saude from Produto inner join composto inner join Elemento on  Produto.marca = composto.prodMarca and Produto.codigo=composto.produto and Elemento.codigo=composto.elemento group by Produto.nome)alias3 where alias2.pior=alias3.pior_para_saude;
 
 --Liste o sexo e a idade de todas as pessoas abrangidas por esta base de dados –
 --consumidores e seus dependentes
